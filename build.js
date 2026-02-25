7eb97eb2eb84a2418a3ef9e88df0ebdef7ebbc2c
Maksim Bartoshyk <maksim_bartoshyk@epam.com>
2026-02-25 20:13:36 +0300

    feat (pwa): add simplest progressive web app version

--- build.js
+++ build.js
@@ -14,8 +14,28 @@ waterfall = waterfall.replace('<script src="./html/control-panel.js"></script>',
 waterfall = waterfall.replace('<script src="./html/startup.js"></script>', '<script>' + fs.readFileSync('./html/startup.js') + '</script>');
 fs.writeFileSync('./offline/waterfall-offline.html', waterfall);
 
+waterfall = waterfall.replace('<!-- REGISTER SERVICE WORKER -->', `
+  <script>
+    if ('serviceWorker' in navigator) { 
+      navigator.serviceWorker.register('./service-worker.js'); 
+    }
+  </script>
+`);
+waterfall = waterfall.replace('<!-- PWA META -->', `
+  <meta name="theme-color" content="#000000">
+  <meta name="mobile-web-app-capable" content="yes">
+  <meta name="apple-mobile-web-app-status-bar-style" content="black">
+  <meta name="apple-mobile-web-app-title" content="WF">
+  <link rel="manifest" href="manifest.json">
+  <link rel="apple-touch-icon" href="icons/icon-192.png"></link>
+`);
+fs.writeFileSync('./pwa/waterfall.html', waterfall);
+
 let rcspg = fs.readFileSync('./rcspg.html', 'utf-8');
 rcspg = rcspg.replace('<script src="./shared/spectrum.js"></script>', '<script>' + fs.readFileSync('./shared/spectrum.js') + '</script>');
 rcspg = rcspg.replace('<script src="./shared/radiacode.js"></script>', '<script>' + fs.readFileSync('./shared/radiacode.js') + '</script>');
 rcspg = rcspg.replace('<script src="./shared/csv.js"></script>', '<script>' + fs.readFileSync('./shared/csv.js') + '</script>');
 fs.writeFileSync('./offline/rcspg-offline.html', rcspg);
+
+
+
